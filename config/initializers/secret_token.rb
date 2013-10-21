f406d0ce3f54176c67bcef777b434d8c0d6252a4
Deniss <den.b@itservicesgroup.co.uk>
2013-10-21 23:00:51 +0100

    ActiveAdm panel test implementation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Activeadmin::Application.config.secret_token = '467a00299c4eaf1cb64b35aac171de10c0928c36ff056e388fbf166493ea95d98bd88a97274d2cb022f101aee7d86bd668b407564181b9884c2a5ef2a2cf13c7'
